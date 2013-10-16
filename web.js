873a9657e7f42097e54c9e990c680db7d778540c
Administrator <momen@ubuntu.(none)>
2013-10-16 21:22:25 +0200

    web.js use index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buffer = new buffer(100);
+var string ;
+buffer = fs.readFileSynch("index.html","utf8");
+string = buf.toString("utf8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
